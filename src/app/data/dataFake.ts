39f472a9f803007f0bcca228a75ac9e705c3d5df
leodario <leodario3@hotmail.com>
2024-06-10 14:24:28 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "NOVO HOMEM DE FERRO EM 3D",
+    "description": "Marvel studios anuncia o novo filme do Homem de Ferro",
+    "photoCover": "https://de9o6n2ujz7l.cloudfront.net/cache/bb/29/bb29887dfe5f7dce75c9ad9d6190e1dc.jpg"
+  },
+  {
+    "id":"2",
+    "title": "Novo filme anunciado",
+    "description": "Novo filme dos vingadores anunciado",
+    "photoCover": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+  {
+    "id":"3",
+    "title": "Novidades no Disney+",
+    "description": "Confira as novidadades do Disney+",
+    "photoCover": "https://i.ytimg.com/vi/JhJqgQR6fM0/maxresdefault.jpg"
+  },
+  {
+    "id":"4",
+    "title": "Quinta temporada da animação Homem de Ferro",
+    "description": "A nova animação do Homem de Ferro promete ter muita ação e surpresas",
+    "photoCover": "https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6659BD74E7A52EE292ACAADEF5FB29A5A62131059C974F6343126EF1E549BFC2/scale?width=1200&aspectRatio=1.78&format=webp"
+  }
+]
